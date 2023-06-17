74d4f2ca8c66af1261157e06828b1fb0d34c45ce
deekshi-hari <deekhari00716@gmail.com>
2023-06-17 22:13:48 +0530

    reset confirm

--- config/settings.py
+++ config/settings.py
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'deekhari00716@gmail.com '
-EMAIL_HOST_PASSWORD = 'oxlwzyfejrgoutdd'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
