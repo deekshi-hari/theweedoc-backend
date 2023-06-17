c95c28cddcb4d86cd3366194d9c70da5895f186e
deekshi-hari <deekhari00716@gmail.com>
2023-06-17 14:59:29 +0530

    remove sms

--- users/services.py
+++ users/services.py
@@ -1,14 +1,14 @@
-from twilio.rest import Client
-from django.conf import settings
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# from twilio.rest import Client
+# from django.conf import settings
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
 
-def send_sms(phone_number, message):
-    client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-    message = client.messages.create(
-        body=message,
-        from_=settings.TWILIO_PHONE_NUMBER,
-        to=phone_number
-    )
+# def send_sms(phone_number, message):
+#     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+#     message = client.messages.create(
+#         body=message,
+#         from_=settings.TWILIO_PHONE_NUMBER,
+#         to=phone_number
+#     )
 
-    return Response({'message': 'SMS sent successfully.'})
+#     return Response({'message': 'SMS sent successfully.'})
