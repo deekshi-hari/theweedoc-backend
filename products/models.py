33938a81b25b52cbe28a9720957dc6489747f8d3
AnjithNair <anjithpkp42@gmail.com>
2024-05-03 20:04:50 +0530

    added new languages table and a languages field to the products table

--- products/models.py
+++ products/models.py
@@ -2,40 +2,47 @@ from django.db import models
 from users.models import User
 from django.utils import timezone
 
+
 class Genere(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField('updated_at', auto_now=True, blank=True, null=True)
+    updated_at = models.DateTimeField(
+        "updated_at", auto_now=True, blank=True, null=True
+    )
 
 
 class ProductManagerActive(models.Manager):
 
     def get_is_active(self):
-        return self.filter(status="approved").order_by('-created_at')
-    
+        return self.filter(status="approved").order_by("-created_at")
+
     def get_inactive(self):
-        return self.filter(is_active=False).order_by('-created_at')
-    
+        return self.filter(is_active=False).order_by("-created_at")
+
+
+class Languages(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+
 
 class Product(models.Model):
     Language_choice = [
-        ('malayalam', 'MALAYALAM'),
-        ('english', 'ENGLISH'),
-        ('tamil', 'TAMIL'),
-        ('hindi', 'HINDI'),
+        ("malayalam", "MALAYALAM"),
+        ("english", "ENGLISH"),
+        ("tamil", "TAMIL"),
+        ("hindi", "HINDI"),
     ]
 
     STATUS = [
-        ('approved', 'APPROVED'),
-        ('rejected', 'REJECTED'),
-        ('pending', 'PENDING'),
+        ("approved", "APPROVED"),
+        ("rejected", "REJECTED"),
+        ("pending", "PENDING"),
     ]
 
     title = models.CharField(max_length=200, unique=True)
-    status = models.CharField(choices=STATUS, max_length=50, default='pending')
+    status = models.CharField(choices=STATUS, max_length=50, default="pending")
     status_reason = models.CharField(max_length=800, blank=True)
     description = models.TextField(blank=True, null=True)
-    language = models.CharField(choices=Language_choice, max_length=200, default='malayalam')
+    languages = models.ForeignKey(Languages, on_delete=models.CASCADE)
     image = models.CharField(max_length=500, blank=True)
     video = models.CharField(max_length=500, blank=True)
     duration = models.FloatField(blank=True, null=True)
@@ -43,10 +50,14 @@ class Product(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     age = models.CharField(max_length=50, blank=True, null=True)
     is_active = models.BooleanField(default=False)
-    likes = models.ManyToManyField(User, related_name='liked_products', blank=True)
-    dislikes = models.ManyToManyField(User, related_name='disliked_products', blank=True)
+    likes = models.ManyToManyField(User, related_name="liked_products", blank=True)
+    dislikes = models.ManyToManyField(
+        User, related_name="disliked_products", blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField('updated_at', auto_now=True, blank=True, null=True)
+    updated_at = models.DateTimeField(
+        "updated_at", auto_now=True, blank=True, null=True
+    )
     objects = models.Manager()  # The default manager
     custom_objects = ProductManagerActive()
 
@@ -83,4 +94,3 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"{self.recipient.username} - {self.content}"
-
